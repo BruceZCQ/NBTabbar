3b0cc9e7c3da44b7372386d400a6b91600f83a2f
BruceZCQ <brucezcq@gmail.com>
2015-04-08 14:10:58 +0800

    create pro

---
+++ NBTarBar/NBTarBar/NBTabBarItem.m
@@ -0,0 +1,21 @@
+//
+//  NBTabBarItem.m
+//  NSTabbar
+//
+//  Created by BruceZCQ on 4/8/15.
+//  Copyright (c) 2015 BruceZCQ. All rights reserved.
+//
+
+#import "NBTabBarItem.h"
+
+@implementation NBTabBarItem
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
